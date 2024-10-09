dc48befabeb8b008ad3f1db8a73eed62f13b5ff3
Fahmi <fanhar30@gmail.com>
2024-10-09 13:09:47 +0700

    Initial commit

---
+++ database/migrations/2024_10_06_235459_create_news_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('news_title', 255);
+            $table->foreignId('author')->constrained('user')->onDelete('set null');
+            $table->text('content');
+            $table->string('image', 255)->nullable();
+            $table->timestamp('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
